335ea08abd8dbff107469bbdf6314ec5681f619e
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-05-24 08:25:03 -0700

    Initial commit

---
+++ src/main/java/com/ravi/microservicesbrewery/MicroservicesBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.microservicesbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/microservicesbrewery/MicroservicesBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.microservicesbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
